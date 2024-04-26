b052e18873824bdfbf96a2fa413fdf6ba24a3ca4
Niha-Wani <niha78781@gmail.com>
2024-04-26 07:47:35 -0700

    First JS

---
+++ index.js
@@ -0,0 +1,10 @@
+var x=0;
+function increment(){
+    x=x+1;
+    document.getElementById("count").innerText=x;
+}
+function save(){
+    document.getElementById("save").textContent += x + " - ";
+    document.getElementById("count").textContent=0;
+    x=0;
+}
