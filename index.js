976aaa6ef1e78dfdcdec1f74b3b326fb8bcac221
Niha Mukhtar Wani <niha78781@gmail.com>
2024-05-08 20:43:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var x=0;
-function increment(){
+function increment()
+{
     x=x+1;
     document.getElementById("count").innerText=x;
 }
-function save(){
+function save()
+{
     document.getElementById("save").textContent += x + " - ";
     document.getElementById("count").textContent=0;
     x=0;
